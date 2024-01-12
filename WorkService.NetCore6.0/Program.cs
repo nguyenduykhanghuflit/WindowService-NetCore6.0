83ef95830b3b28ccfa9e74e99b1b90bdea4e04c9
NguyenDuyKhang <nguyenduykhanghuflit@gmail.com>
2024-01-12 10:09:27 +0700

    init

---
+++ WorkService.NetCore6.0/Program.cs
@@ -0,0 +1,10 @@
+using WorkService.NetCore6._0;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
